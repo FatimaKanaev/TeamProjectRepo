2a982521b631456da7625ad522d8ccb7a5a82cf9
%username% <%email%>
2025-04-29 13:54:22 -0400

    MAin character moving codes

--- MainCharacter.java
+++ MainCharacter.java
@@ -12,8 +12,40 @@ public class MainCharacter extends Actor
      * Act - do whatever the MainCharacter wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act()
+    public MainCharacter()
     {
-        // Add your action code here.
+        setImage("MainCharacterRight.png");
     }
+
+    public void act() 
+    {
+        if (Greenfoot.isKeyDown("up")) {
+            setImage("MainCharacterUp.png");
+            moveUp();
+        } 
+        else if (Greenfoot.isKeyDown("down")) {
+            setImage("MainCharacterRight.png");
+            moveDown();
+        }
+        else if (Greenfoot.isKeyDown("left")) {
+            setImage("MainCharacterLeft.png");
+            move(-2);
+        } 
+        else if (Greenfoot.isKeyDown("right")) {
+            setImage("MainCharacterRight.png");
+            move(2);
+        } 
+        else {
+            setImage("MainCharacterRight.png");
+        }
+    }
+    
+    private void moveDown() {
+        setLocation(getX(), getY() + 5);
+    }
+    
+    private void moveUp() {
+        setLocation(getX(), getY() - 5);
+    }
+
 }
